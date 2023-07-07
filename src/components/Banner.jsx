b71be4f6580b49b95f796b13ea92d45c26e277a8
Master Pampa <styven.munhurrun@gmail.com>
2023-07-07 11:42:02 +0200

    banniere responsive a propos

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,14 +4,15 @@ import banner2 from '../assets/banner2.png'
 import '../styles/banner.css'
 
 function Banner({ currentPage }) {
-    let image = banner;
-  
+    let image = banner
+    let bannerClass = "banner"
     if (currentPage === 'about') {
-      image = banner2;
+      image = banner2
+      bannerClass = "banner2"
     }
     
     return (
-        <div className='banner'>
+        <div className={bannerClass}>
             <img src={image} alt="Banniere" />
             <div className='banner__title'>
                 <h1>Chez vous, partout et ailleurs</h1>
